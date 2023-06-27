25a40dbdce2930f3890daeb15bbd1623063a0862
Denis <denis-boz@hotmail.com>
2023-06-27 09:14:50 +0300

    Решение задачи 56

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@
 8 4 4 2
 */
 
+/*
 Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -67,4 +68,80 @@ void ChangedMatrix(int[,] array)
     }
   }
 }
+*/
 
+/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+if (rows != columns)
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+
+int[,] resultMatrix = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(resultMatrix);
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = new Random().Next(min, max + 1);
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+} 
+
+int GetRowsSum(int[,] array, int i)
+{
+int sum = array[i,0];
+for (int j = 1; j < array.GetLength(1); j++)
+{
+sum += array[i,j];
+}
+return sum;
+}
+
+int minSum = 1;
+int sum = GetRowsSum(resultMatrix, 0);
+for (int i = 1; i < resultMatrix.GetLength(0); i++)
+{
+if (sum > GetRowsSum(resultMatrix, i))
+{
+sum = GetRowsSum(resultMatrix, i);
+minSum = i+1;
+}
+}
+Console.WriteLine($"Строка c наименьшей суммой элементов: {minSum}");
